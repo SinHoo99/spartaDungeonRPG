6a8985ff43f5c1bb86b3e36e69b3dc9ac26fc7ea
SinHoo99 <tjdqls991218@gmail.com>
2024-04-24 19:07:37 +0900

    spartaDungeon

--- ConsoleRPG/obj/Release/net8.0/ConsoleRPG.AssemblyInfo.cs
+++ ConsoleRPG/obj/Release/net8.0/ConsoleRPG.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleRPG")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5372be29855a1dffd5a889f5fa2658f7813b7633")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleRPG")]
